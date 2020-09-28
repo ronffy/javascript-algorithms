e0404015ac2070ca46ed00524ae165f598e2c465
ronffy <ronffy@sina.com>
2020-09-28 18:07:03 +0800

    fix: webpack entry

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-  entry: './src/index.ts',
+  entry: './example/index.ts',
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
