1e0b8d809aa249030abc222db1b0beb5815c4863
ronffy <ronffy@sina.com>
2020-09-28 18:05:19 +0800

    chore: add tree 示例

--- example/tree.ts
+++ example/tree.ts
@@ -1,12 +1,12 @@
 /**
- * @description 
+ * @description 树相关示例
  * @author ronffy
  * @Date 2020-09-25 15:12:18
- * @LastEditTime 2020-09-28 17:53:11
+ * @LastEditTime 2020-09-28 18:04:41
  * @LastEditors ronffy
  */
-import BinarySearchTree from './tree/BinarySearchTree'
-import AVLTree from './tree/AVLTree'
+import BinarySearchTree from '../src/tree/BinarySearchTree'
+import AVLTree from '../src/tree/AVLTree'
 
 function insert(tree) {
   tree.insert(3)
@@ -26,48 +26,6 @@ function insert(tree) {
 const binarySearchTree = new BinarySearchTree();
 
 insert(binarySearchTree)
-
-
-//         3
-//     2       4
-//   1           9 
-//            6     11 
-//           5  7      17
-//                   15
-//                 13
-
-
-
-
-//          7
-//       6     9
-//     3          11
-//   2                17
-// 1                15
-//                13
-
-
-
-//      3
-//   2       6
-// 1     4       11    
-//        5    9   15
-//           7    13 17
-
-
-
-
-
-//       6
-//   3        11
-//  2  4    9   15
-// 1    5  7   13 17
-
-
-
-
-
-
 
 // console.log(binarySearchTree.root);
 // console.log('binarySearchTree', JSON.stringify(binarySearchTree.root));
